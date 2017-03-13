03a2f972fe7f627cfb513a343df3fba7591c2be6
juniornelson123 <junior123nelson@gmail.com>
2017-03-13 11:33:40 -0300

    adicionando seed status

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	has_many :products
 	has_many :clients
+	belong_to :status
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "*******Criando status******"
+Status.create([{description: "active"}, {description: "inactive"}, {description: "pending"}, {description: "deleted"}])
